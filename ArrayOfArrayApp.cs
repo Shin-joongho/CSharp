11f54faa1c90349598b2fa879cd6e9b9c57d880c
Shin-joongho <80227756+Shin-joongho@users.noreply.github.com>
2021-03-15 10:38:14 +0900

    Add files via upload

---
+++ ArrayOfArrayApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class ArrayOfArrayApp
+    {
+        static void Main(string[] args)
+        {
+            int[][] arrayOfarray = new int[3][];
+            int i, j;
+            for (i = 0; i < arrayOfarray.Length; i++)
+                arrayOfarray[i] = new int[i + 3];
+            for (i = 0; i < arrayOfarray.Length; i++)
+                for (j = 0; j < arrayOfarray[i].Length; j++)
+                    arrayOfarray[i][j] = i * arrayOfarray[i].Length + j;
+            for (i = 0; i < arrayOfarray.Length; i++)
+            {
+                for (j = 0; j < arrayOfarray[i].Length; j++)
+                    Console.Write(" " + arrayOfarray[i][j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
